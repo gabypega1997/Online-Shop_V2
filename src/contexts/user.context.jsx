a23bf2574466665ca0887ca728a6eac3557946e7
gabypega1997 <gabypega1997@gmail.com>
2023-02-19 12:06:09 +0100

    optimized the user.context to use reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,25 +1,55 @@
-import { createContext, useState, useEffect } from "react";
-import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
+import { createContext, useEffect, useReducer } from "react";
+import {
+    onAuthStateChangedListener,
+    createUserDocumentFromAuth,
+} from "../utils/firebase/firebase.utils";
 //as the actual value you want to access
 export const UserContext = createContext({
     currentUser: null,
     setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload,
+            };
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+};
+
+const INITIAL_STATE = {
+    currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    const [{ currentUser }, dispach] = useReducer(userReducer, INITIAL_STATE);
+    console.log(currentUser);
+    const setCurrentUser = (user) => {
+        dispach({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    };
+
     const value = { currentUser, setCurrentUser };
 
     useEffect(() => {
         const unsubscribe = onAuthStateChangedListener((user) => {
-            if(user){
+            if (user) {
                 createUserDocumentFromAuth(user);
             }
             setCurrentUser(user);
         });
 
         return unsubscribe;
-    });
+    }, []);
     return (
         <UserContext.Provider value={value}>{children}</UserContext.Provider>
     );
