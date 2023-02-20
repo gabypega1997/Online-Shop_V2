f5b84e79386d1e2351f25754bf1fff4e4c7fbc79
gabypega1997 <gabypega1997@gmail.com>
2023-02-20 19:12:34 +0100

    apply the category redux instead of the context

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,16 +1,18 @@
-import { useContext, useState, useEffect, Fragment } from "react";
+import { useState, useEffect, Fragment } from "react";
 
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-import { CategoriesContext } from "../../contexts/categories.context";
 
 import ProductCart from "../../components/product-card/product-card.component";
 
 import { CategoryContainer, CategoryTitle } from "./category.styles.jsx";
+import { CategoriesSelector } from "../../store/categories/category.selector";
 
 const Category = () => {
     const { category } = useParams();
-    const { categoriesMap } = useContext(CategoriesContext);
+    const categoriesMap = useSelector(CategoriesSelector)
+
     const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(() => {
---
+++ src/store/categories/category.selector.js
@@ -0,0 +1,1 @@
+export const CategoriesSelector = (state) => state.categories.categoriesMap;
