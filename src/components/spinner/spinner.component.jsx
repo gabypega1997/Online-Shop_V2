1fb9b1b2c7338b4ed6e5ca8aa279898f28014fb4
gabypega1997 <gabypega1997@gmail.com>
2023-02-25 14:41:23 +0100

    create a new actions for three new action to the categories

---
+++ src/components/spinner/spinner.component.jsx
@@ -0,0 +1,9 @@
+import { SpinnerContainer, SpinnerOverlay } from "./spinner.styles";
+
+const Spinner = () => (
+    <Spinner>
+        <SpinnerOverlay />
+    </Spinner>
+);
+
+export default Spinner;
