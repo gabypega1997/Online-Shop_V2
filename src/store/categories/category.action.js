58c58d1444bfb39402d05990db9c9f1011bb9fac
gabypega1997 <gabypega1997@gmail.com>
2023-02-25 11:15:39 +0100

    add redux thunk to a shop component

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,5 +1,25 @@
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import { createAction } from "../../utils/reducer/reducer.utils";
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
-export const setCategoriesMap = (categoriesMap) =>
-    createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP, categoriesMap);
+export const fetchCategoriesStart = () =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categoriesMap) =>
+    createAction(
+        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+        categoriesMap
+    );
+
+export const fetchCategoriesFaild = (error) =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+    try {
+        const categoryMap = await getCategoriesAndDocuments();
+        dispatch(fetchCategoriesSuccess(categoryMap));
+    } catch (error) {
+        dispatch(fetchCategoriesFaild(error));
+    }
+};
