842db2638b878ddd8cf4f7fce654478048896bd8
gabypega1997 <gabypega1997@gmail.com>
2023-02-25 10:35:36 +0100

    change the category.types to use a async redux

--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,8 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./root-reducer";
 
+import thunk from "redux-thunk";
+
 const persistConfig = {
     key: "root",
     storage,
@@ -12,11 +14,17 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
-    Boolean
-);
+const middleWares = [
+    process.env.NODE_ENV === "development" && logger,
+    thunk,
+].filter(Boolean);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer =
+    (process.env.NODE_ENV !== "production" &&
+        window &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
     persistedReducer,
